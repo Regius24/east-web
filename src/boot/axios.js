a84574eb4cee9609992d04de2e1c833079297bc7
Alfonso Tristan Jay Arias <AMArias@smart.com.ph>
2021-01-07 16:58:19 +0800

    first init

---
+++ src/boot/axios.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+// const DEV = "http://localhost:64575/";
+const PROD = location.origin + '/west2/'
+
+if (process.env.PROD) axios.defaults.baseURL = PROD
+// axios.defaults.withCredentials = true
+
+// const axiosInstance = axios.create({
+//   withCredentials: true,
+//   baseURL: process.env.DEV ? DEV : PROD
+// });
+
+Vue.prototype.$axios = axios
+
+export { axios }
