95a43cbfbe6d6b4358558dd38493559b9a39dbb8
Alfonso Tristan Jay Arias <AMArias@smart.com.ph>
2021-01-08 13:33:49 +0800

    added bg

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    asd
+  <div class="window-height page_bg">
+    <div class="flex flex-center fit">
+      <q-card>
+        <q-card-section>
+          WEST
+        </q-card-section>
+      </q-card>
+    </div>
   </div>
 </template>
 
@@ -9,3 +15,10 @@ export default {
   name: 'PageIndex'
 }
 </script>
+
+<style lang="scss" scoped>
+.page_bg {
+  background: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('../assets/bg/photo-1543946602-a0fce8117697.jpg');
+  background-size: cover;
+}
+</style>
