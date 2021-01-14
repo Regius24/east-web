4f1b7e4d4e31da0e8bae4744337d209ef8b6cf4f
Alfonso Tristan Jay Arias <AMArias@smart.com.ph>
2021-01-14 12:41:31 +0800

    updated axios

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import axios from 'axios'
 
 // const DEV = "http://localhost:64575/";
-const PROD = location.origin + '/west2/'
+const PROD = location.origin + location.pathname
 
 if (process.env.PROD) axios.defaults.baseURL = PROD
 // axios.defaults.withCredentials = true
