c1c50b99020e87ef0e1329cccc13ca607c5a50d6
Alfonso Tristan Jay Arias <AMArias@smart.com.ph>
2021-04-08 17:42:47 +0800

    added uploader for onehub report

--- src/components/report/onehub/uploader.vue
+++ src/components/report/onehub/uploader.vue
@@ -1,12 +1,61 @@
 <template>
-  <div>My component</div>
+  <q-dialog
+    ref="dialog"
+    @hide="onDialogHide"
+  >
+    <q-card class="q-dialog-plugin overflow-hidden">
+      <q-card-section>
+        <q-uploader
+          class="fit"
+          :url=uploader.path
+          :accept=uploader.types
+          color="teal"
+          ref="daUploader"
+          auto-upload
+          flat
+          @uploaded=hasuploaded
+        />
+      </q-card-section>
+    </q-card>
+  </q-dialog>
 </template>
 
 <script>
+// import { notify } from 'boot/notifier'
+// import PostRepo from 'src/repository/post'
+
 export default {
-  // name: 'ComponentName',
   data () {
-    return {}
+    return {
+      file: null,
+      loading: false,
+      uploader: {
+        model: false,
+        path: this.$axios.defaults.baseURL + 'api/post/rawdata',
+        types: '.csv'
+      }
+    }
+  },
+
+  methods: {
+    hasuploaded () { },
+
+    show () {
+      this.$refs.dialog.show()
+    },
+    hide () {
+      this.$refs.dialog.hide()
+    },
+    onDialogHide () {
+      this.$emit('hide')
+    },
+    onOKClick () {
+      this.$emit('ok')
+      this.hide()
+    },
+    onCancelClick () {
+      this.hide()
+    }
   }
 }
 </script>
--- src/pages/ReportOnehub.vue
+++ src/pages/ReportOnehub.vue
@@ -1,11 +1,85 @@
 <template>
   <q-page padding>
-    <!-- content -->
+    <div class="row justify-center">
+      <div class="col-12">
+        <TABLE />
+      </div>
+    </div>
+
+    <!-- UPLOAD BUTTON -->
+    <q-page-sticky
+      position="bottom-right"
+      :offset="fabPos"
+    >
+      <q-btn
+        fab
+        icon="mdi-file-upload"
+        color="accent"
+        :disable="draggingFab"
+        :style="showUploader ? '' : 'display: none;'"
+        v-touch-pan.prevent.mouse="moveFab"
+        @click="openUploader"
+      />
+    </q-page-sticky>
   </q-page>
 </template>
 
 <script>
+import GET from 'src/repository/get'
+import { first } from 'lodash'
+import { notify } from 'boot/notifier'
+
 export default {
-  // name: 'PageName',
+  name: 'ReportOnehub',
+
+  components: {
+    TABLE: () => import('components/report/onehub/table')
+  },
+
+  data () {
+    return {
+      showUploader: false,
+      fabPos: [18, 18],
+      draggingFab: false
+    }
+  },
+
+  methods: {
+    moveFab (ev) {
+      this.draggingFab = ev.isFirst !== true && ev.isFinal !== true
+
+      this.fabPos = [
+        this.fabPos[0] - ev.delta.x,
+        this.fabPos[1] - ev.delta.y
+      ]
+    },
+    openUploader () {
+      this.$q.dialog({
+        component: () => import('components/report/onehub/uploader'),
+        parent: this
+      }).onOk(() => {
+        console.log('OK')
+      }).onCancel(() => {
+        console.log('Cancel')
+      }).onDismiss(() => {
+        console.log('Called on OK or Cancel')
+      })
+    }
+  },
+
+  async beforeMount () {
+    try {
+      const { data: user } = await GET.UserProfile(this.$q.localStorage.getItem('userAccnt'))
+
+      this.showUploader = first(user).upload
+    } catch (err) {
+      console.log(err)
+      notify('Something went wrong', '', 'mdi-alert', 'red')
+    }
+  },
+
+  mounted () {
+    notify('Fetching Data', 'Please wait while data loads', 'mdi-timer-sand', 'orange')
+  }
 }
 </script>
