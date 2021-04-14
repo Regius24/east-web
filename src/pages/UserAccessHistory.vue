70bdb7d5acf3868c8ed3dcc51e3bd099e766e651
Alfonso Tristan Jay Arias <AMArias@smart.com.ph>
2021-04-14 20:07:10 +0800

    updated uam history

--- src/pages/UserAccessHistory.vue
+++ src/pages/UserAccessHistory.vue
@@ -19,11 +19,9 @@
 </template>
 
 <script>
-// import { date } from 'quasar'
-// import { mapActions } from 'vuex'
-// import { flatten, concat, sortBy, indexOf } from 'lodash'
-// import jsonata from 'jsonata'
-// import GetRepo from 'src/repository/get'
+import GetRepo from 'src/repository/get'
+import jsonata from 'jsonata'
+import { first, includes, flatten, sortBy, indexOf } from 'lodash'
 import { notify } from 'boot/notifier'
 
 export default {
@@ -32,6 +30,78 @@ export default {
   components: {
     SUMMARY: () => import('components/user-access-history/summary'),
     TABLE: () => import('components/user-access-history/table')
+  },
+
+  data () {
+    return {
+      brandList: [],
+      profileType: '',
+      vendorType: '',
+
+      smartDate: '',
+      pldtDate: '',
+      smartDateList: [],
+      pldtDateList: [],
+
+      smartSummary: [],
+      pldtSummart: []
+    }
+  },
+
+  methods: {
+    initFetchData () {
+      if (includes(this.brandList, 'Pldt')) this.fetchDates('pldt', 'p')
+      if (includes(this.brandList, 'Smart')) this.fetchDates('smart', 's')
+    },
+    async fetchDates (brand, type) {
+      const { data: dates } = await GetRepo.UamDataHistoryDates(brand, type)
+      this[`${brand}DateList`] = dates
+      this[`${brand}Date`] = first(dates).date
+
+      this.fetchSummaryData(brand, this.vendorType, '%')
+    },
+    async fetchSummaryData (brand, vendor, site) {
+      let { data: summary } = await GetRepo.UamDataSummaryHistory(this[`${brand}Date`], brand, vendor, site)
+
+      const tableOrder = ['ACTIVE', 'TRAINEES', 'INACTIVE', 'RESIGNED']
+      const expression = jsonata(`
+          $ { Table: $ } ~> $each(function($v1, $k1){
+            $v1 { Lob: $ } ~> $each(function($v2, $k2){
+                {
+                    'Table': $distinct($v2.Table),
+                    'Date': $distinct($v2.Date),
+                    'Name': $k2,
+                    'Agents': $sum($v2.Agents),
+                    'Complete': $sum($v2.Complete),
+                    'Percent': $round(($sum($v2.Complete)/$sum($v2.Agents)) * 100, 2),
+                    '_children': $v2 {
+                        Vendor: $
+                    } ~> $each(function($v3, $k3) {
+                        {
+                            'Name': $k3,
+                            'LockedFte': $v3.LockedFte,
+                            'Agents': $sum($v3.Agents),
+                            'Complete': $sum($v3.Complete),
+                            'Percent': $round(($sum($v3.Complete)/$sum($v3.Agents)) * 100, 2)
+                        }
+                    })
+                }
+            })
+          })
+        `)
+      summary = expression.evaluate(sortBy(summary, obj => indexOf(tableOrder, obj.Table)))
+      this[`${brand}Summary`] = flatten(summary)
+    }
+  },
+
+  async beforeMount () {
+    const { data: user } = await GetRepo.UserProfile(this.$q.localStorage.getItem('userAccnt'))
+
+    this.brandList = first(user).brand.split(',').map(m => m.replace(/(^|\s)\S/g, l => l.toUpperCase()))
+    this.profileType = first(user).profile
+    this.vendorType = first(user).profile === 'admin' ? '%' : first(user).vendor
+
+    this.initFetchData()
   },
 
   mounted () {
--- src/repository/get.js
+++ src/repository/get.js
@@ -5,14 +5,20 @@ const resource = 'api/get/'
 export default {
   ValidateUser: (USERNAME, PASSWORD) => axios.get(`${resource}ValidateUser?username=${USERNAME}&password=${PASSWORD}`),
   UserProfile: (USERNAME) => axios.get(`${resource}UserProfile?username=${USERNAME}`),
-  UamDataRaw: (BRAND, VENDOR) => axios.get(`${resource}UamDataRaw?brand=${BRAND}&vendor=${VENDOR}`),
+
   UamDataSummary: (BRAND, VENDOR, SITE) => axios.get(`${resource}UamDataSummary?brand=${BRAND}&vendor=${VENDOR}&site=${SITE}`),
   UamDataAgents: (BRAND, VENDOR) => axios.get(`${resource}UamDataAgents?brand=${BRAND}&vendor=${VENDOR}`),
   UamDataAgentsDetailed: ({ BRAND, LOB, VENDOR, TABLE }) => axios.get(`${resource}UamDataAgentsDetailed?brand=${BRAND}&lob=${LOB}&vendor=${VENDOR}&table=${TABLE}`),
   UamDataAgentsDistinctCol: (BRAND, CATEGORY) => axios.get(`${resource}UamDataAgentsDistinctCol?brand=${BRAND}&category=${CATEGORY}`),
+
+  UamDataHistoryDates: (BRAND, TYPE) => axios.get(`${resource}UamDataHistoryDates?brand=${BRAND}&type=${TYPE}`),
+  UamDataSummaryHistory: (DATE, BRAND, VENDOR, SITE) => axios.get(`${resource}UamDataSummaryHistory?date=${DATE}&brand=${BRAND}&vendor=${VENDOR}&site=${SITE}`),
+
   IrabData: (VENDOR) => axios.get(`${resource}IrabData?vendor=${VENDOR}`),
   IrabDataSummary: (MONTH, VENDOR) => axios.get(`${resource}IrabDataSummary?month=${MONTH}&vendor=${VENDOR}`),
+
   PasswordData: () => axios.get(`${resource}PasswordData`),
   PasswordDataSummary: (MONTH) => axios.get(`${resource}PasswordDataSummary?month=${MONTH}`),
+
   OnehubData: () => axios.get(`${resource}OnehubData`)
 }
