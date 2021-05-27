3bac2a30018e02f2181a8b31e03db36d548d1399
Alfonso Tristan Jay Arias <AMArias@smart.com.ph>
2021-05-27 11:19:09 +0800

    updated get repo and components for user access summary

--- src/components/user-access/summary/summary.vue
+++ src/components/user-access/summary/summary.vue
@@ -1,12 +1,101 @@
 <template>
-  <div>My component</div>
+  <q-card>
+    <q-card-section>
+      <q-table
+        dense
+        flat
+        bordered
+        :title="title"
+        :title-class="titleClass"
+        row-key="name"
+        separator="cell"
+        color="accent"
+        virtual-scroll
+        style="max-height: 50vh;"
+        :data="data"
+        :columns="columns"
+        :filter="filter"
+        :pagination.sync="pagination"
+        :rows-per-page-options="[0]"
+        :loading="data.length > 0 ? false : true"
+      >
+        <template v-slot:top-right>
+          <!-- BUTTONS -->
+          <q-btn-group class="q-mr-sm">
+            <q-btn
+              outline
+              color="accent"
+              label="CSV"
+              @click="exportData"
+            />
+          </q-btn-group>
+
+          <!-- SEARCH -->
+          <q-input
+            borderless
+            dense
+            debounce="300"
+            v-model="filter"
+            placeholder="Search"
+          >
+            <template v-slot:append>
+              <q-icon name="search" />
+            </template>
+          </q-input>
+        </template>
+      </q-table>
+    </q-card-section>
+  </q-card>
 </template>
 
 <script>
+import { first } from 'lodash'
+import { exportFile } from 'quasar'
+import { unparse } from 'papaparse'
+import { notify } from 'boot/notifier'
+
 export default {
-  // name: 'ComponentName',
+  props: ['title', 'titleClass', 'data'],
+
   data () {
-    return {}
+    return {
+      columns: [],
+      filter: '',
+      pagination: {
+        rowsPerPage: 0
+      }
+    }
+  },
+
+  watch: {
+    data (val) {
+      const cols = Object
+        .keys(first(val))
+        .map(col => {
+          return {
+            name: col,
+            field: col,
+            label: col,
+            style: 'max-width: 300px;',
+            classes: 'ellipsis',
+            headerClasses: 'text-weight-bolder'
+          }
+        })
+
+      this.columns = cols
+    }
+  },
+
+  methods: {
+    exportData () {
+      const title = `Summarized Data for ${this.title}`
+      this.export(title, unparse(this.data))
+    },
+
+    export (name, data) {
+      notify('Downloading Data', 'Please wait', 'mdi-download', 'blue')
+      exportFile(`${name}.csv`, data)
+    }
   }
 }
 </script>
--- src/pages/UserAccessSummary.vue
+++ src/pages/UserAccessSummary.vue
@@ -1,11 +1,128 @@
 <template>
   <q-page padding>
-    <!-- content -->
+    <!-- SUMMARIES -->
+    <div class="row justify-center q-col-gutter-md">
+      <!-- PLDT -->
+      <div class="col-12">
+        <SUMMARY
+          :title="'Summarized PLDT Data'"
+          :titleClass="'text-red'"
+          :data="pldtData"
+        />
+      </div>
+
+      <!-- SMART -->
+      <div class="col-12">
+        <SUMMARY
+          :title="'Summarized SMART Data'"
+          :titleClass="'text-green'"
+          :data="smartData"
+        />
+      </div>
+    </div>
+
+    <!-- UPLOAD BUTTON -->
+    <q-page-sticky
+      position="bottom-right"
+      :offset="fabPos"
+    >
+      <q-btn
+        fab
+        icon="mdi-file-upload"
+        color="accent"
+        :disable="draggingFab"
+        :style="showUploader ? '' : 'display: none;'"
+        v-touch-pan.prevent.mouse="moveFab"
+        @click="openUploader"
+      />
+    </q-page-sticky>
   </q-page>
 </template>
 
 <script>
+import GetRepo from 'src/repository/get'
+import { notify } from 'boot/notifier'
+
 export default {
-  // name: 'PageName',
+  name: 'UserAccessSummary',
+
+  components: {
+    SUMMARY: () => import('components/user-access/summary/summary')
+  },
+
+  data () {
+    return {
+      profileType: '',
+      vendorType: '',
+      brandList: [],
+
+      showUploader: false,
+      fabPos: [18, 18],
+      draggingFab: false,
+
+      pldtData: [],
+      smartData: []
+    }
+  },
+
+  methods: {
+    moveFab (ev) {
+      this.draggingFab = ev.isFirst !== true && ev.isFinal !== true
+
+      this.fabPos = [
+        this.fabPos[0] - ev.delta.x,
+        this.fabPos[1] - ev.delta.y
+      ]
+    },
+
+    openUploader () {
+      this.$q.dialog({
+        component: () => import('components/user-access/per-lob/uploader'),
+        parent: this
+      }).onOk(() => {
+        console.log('OK')
+      }).onCancel(() => {
+        console.log('Cancel')
+      }).onDismiss(() => {
+        console.log('Called on OK or Cancel')
+      })
+    },
+
+    async initializeData (brand, vendor, site) {
+      const { data } = await GetRepo.UamDataSummaryAll(brand, vendor, site)
+
+      this[`${brand}Data`] = data
+      console.log(this[`${brand}Data`])
+    },
+
+    fetchData () {
+      const vendor = this.profileType === 'admin' ? '%' : this.vendorType
+      const site = '%'
+
+      this.brandList.forEach(brand => {
+        this.initializeData(brand.toLowerCase(), vendor, site)
+      })
+    }
+  },
+
+  async beforeMount () {
+    try {
+      const { data } = await GetRepo.UserProfile(this.$q.localStorage.getItem('userAccnt'))
+
+      this.brandList = data[0].brand.split(',').map(m => m.replace(/(^|\s)\S/g, l => l.toUpperCase()))
+      this.profileType = data[0].profile
+      this.vendorType = data[0].vendor
+      this.showUploader = data[0].upload
+
+      this.fetchData()
+    } catch (err) {
+      console.log(err)
+      notify('Something went wrong', '', 'mdi-alert', 'red')
+    }
+  },
+
+  mounted () {
+    notify('Fetching Data', 'Please wait while data loads', 'mdi-timer-sand', 'orange')
+  }
 }
 </script>
