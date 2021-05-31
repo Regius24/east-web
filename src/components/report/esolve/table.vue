f8fa48028b7d5a7b81a68d9de6d18ac26dff53cf
Alfonso Tristan Jay Arias <AMArias@smart.com.ph>
2021-05-31 14:39:53 +0800

    updated esolve table

--- src/components/report/esolve/table.vue
+++ src/components/report/esolve/table.vue
@@ -1,12 +1,102 @@
 <template>
-  <div>My component</div>
+  <q-card>
+    <!-- TABLE -->
+    <q-card-section>
+      <q-table
+        dense
+        flat
+        bordered
+        title="Onehub List"
+        row-key="name"
+        separator="cell"
+        color="accent"
+        virtual-scroll
+        style="max-height: 80vh;"
+        :data="data"
+        :columns="columns"
+        :filter="filter"
+        :pagination.sync="pagination"
+        :rows-per-page-options="[0]"
+        :loading="data.length > 0 ? false : true"
+      >
+        <template v-slot:top-right>
+          <!-- BUTTONS -->
+          <!-- <q-btn-group class="q-mr-sm">
+            <q-btn
+              outline
+              color="accent"
+              label="CSV"
+              @click="exportData"
+            />
+          </q-btn-group> -->
+
+          <!-- SEARCH -->
+          <q-input
+            borderless
+            dense
+            debounce="300"
+            v-model="filter"
+            placeholder="Search"
+          >
+            <template v-slot:append>
+              <q-icon name="search" />
+            </template>
+          </q-input>
+        </template>
+      </q-table>
+    </q-card-section>
+  </q-card>
 </template>
 
 <script>
+import { first } from 'lodash'
+import { exportFile } from 'quasar'
+import { unparse } from 'papaparse'
+import { notify } from 'boot/notifier'
+
 export default {
-  // name: 'ComponentName',
+  name: 'Table',
+
+  props: ['data'],
+
   data () {
-    return {}
+    return {
+      filter: '',
+      columns: [],
+      pagination: {
+        rowsPerPage: 0
+      }
+    }
+  },
+
+  watch: {
+    data (val) {
+      const cols = Object
+        .keys(first(val))
+        .map(col => {
+          return {
+            name: col,
+            field: col,
+            label: col.toUpperCase(),
+            style: 'max-width: 300px;',
+            classes: 'ellipsis'
+          }
+        })
+
+      this.columns = cols
+    }
+  },
+
+  methods: {
+    exportData () {
+      const title = 'Onehub List'
+      this.export(title, unparse(this.data))
+    },
+
+    export (name, data) {
+      notify('Downloading Data', 'Please wait', 'mdi-download', 'blue')
+      exportFile(`${name}.csv`, data)
+    }
   }
 }
 </script>
--- src/pages/ReportEsolve.vue
+++ src/pages/ReportEsolve.vue
@@ -1,11 +1,54 @@
 <template>
   <q-page padding>
-    <!-- content -->
+    <div class="row justify-center">
+      <div class="col-12">
+        <TABLE :data="raw" />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
+import GET from 'src/repository/get'
+import { first } from 'lodash'
+import { notify } from 'boot/notifier'
+
 export default {
-  // name: 'PageName',
+  name: 'ReportESolve',
+
+  components: {
+    TABLE: () => import('components/report/esolve/table')
+  },
+
+  data () {
+    return {
+      showUploader: false,
+      fabPos: [18, 18],
+      draggingFab: false,
+
+      raw: []
+    }
+  },
+
+  methods: {
+
+  },
+
+  async beforeMount () {
+    try {
+      const { data: user } = await GET.UserProfile(this.$q.localStorage.getItem('userAccnt'))
+      const { data: raw } = await GET.EsolveReportData()
+
+      this.showUploader = first(user).upload
+      this.raw = raw
+    } catch (err) {
+      console.log(err)
+      notify('Something went wrong', '', 'mdi-alert', 'red')
+    }
+  },
+
+  mounted () {
+    notify('Fetching Data', 'Please wait while data loads', 'mdi-timer-sand', 'orange')
+  }
 }
 </script>
