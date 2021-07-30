de7ceb8ca144125c6ef4dffd3209c66143f30e43
Alfonso Tristan Jay Arias <AMArias@smart.com.ph>
2021-07-30 14:21:46 +0800

    change electron window size

--- src-electron/main-process/electron-main.js
+++ src-electron/main-process/electron-main.js
@@ -21,14 +21,15 @@ function createWindow () {
    * Initial window options
    */
   mainWindow = new BrowserWindow({
-    width: 1000,
+    width: 1200,
     height: 600,
     useContentSize: true,
+    autoHideMenuBar: true,
     webPreferences: {
       // Change from /quasar.conf.js > electron > nodeIntegration;
       // More info: https://quasar.dev/quasar-cli/developing-electron-apps/node-integration
       nodeIntegration: process.env.QUASAR_NODE_INTEGRATION,
-      nodeIntegrationInWorker: process.env.QUASAR_NODE_INTEGRATION,
+      nodeIntegrationInWorker: process.env.QUASAR_NODE_INTEGRATION
 
       // More info: /quasar-cli/developing-electron-apps/electron-preload-script
       // preload: path.resolve(__dirname, 'electron-preload.js')
